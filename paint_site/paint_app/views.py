53cbd9c821da44c652d88da8764f7743ca8ca5c9
Mikayla Munn <mikayla.munn@gmail.com>
2022-11-17 20:25:59 -0500

    Changed views.py

--- paint_site/paint_app/views.py
+++ paint_site/paint_app/views.py
@@ -38,15 +38,4 @@ class ColorPickerView(View):
 
         return render(request=request, template_name="paint.html", context=context)
 
-    def get(self, request):
-        """Present the color picker form and color the user"""
-        form = ColorPickerForm()
-
-        context = {
-            "form": form,
-            "red": 255,
-            "green": 255,
-            "blue": 255,
-        }
-
-        return render(request=request, template_name="paint.html", context=context)
+    
